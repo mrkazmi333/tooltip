471489579dec681337dd830a33f2af2e8ff9f69f
mrkcancode <motasimkazmi333@gmail.com>
2020-08-09 22:05:04 +0530

    Updates

---
+++ precache-manifest.cd0b946ab25ca4fd381a342564cbdfb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80a941ba9822e08d1fee5df5d9aaf1b",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "3fa65d10470ef06937cf",
+    "url": "/tooltip/static/css/main.5149811a.chunk.css"
+  },
+  {
+    "revision": "39f2c1537d206e9168ea",
+    "url": "/tooltip/static/js/2.0e2e7a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip/static/js/2.0e2e7a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa65d10470ef06937cf",
+    "url": "/tooltip/static/js/main.1954d56f.chunk.js"
+  },
+  {
+    "revision": "6d81586e19be32d511a4",
+    "url": "/tooltip/static/js/runtime-main.ca1d1748.js"
+  }
+]);
